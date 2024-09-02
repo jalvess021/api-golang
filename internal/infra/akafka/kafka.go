ca556cd23d6abc35efea442c27e8123e9cdbb207
jalvess021 <jalvess021@gmail.com>
2024-09-02 17:21:20 -0300

    Feat: consumindo mensagens com kafka

---
+++ internal/infra/akafka/kafka.go
@@ -0,0 +1,27 @@
+package akafka
+
+import(
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+// Ler e receber dados do Kafka
+func Consume(topics []string, servers string, msgChan chan *kafka.Message)  {
+	kafkaConsumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"boostrap.servers": servers,
+		"group.id": "api-golang",
+		"auto.ooffset.reset": "earliest",
+	})
+	
+	if err != nil {
+		panic(err)
+	}
+
+	//Pegar as mensagens e jogar no canal de mensagens
+	kafkaConsumer.SubscribeTopics(topics, nil)
+	for{
+		//-1 timeOut negativo
+		if msg, err := kafkaConsumer.ReadMessage(-1); err == nil {
+			msgChan <- msg
+		}
+	}
+}
