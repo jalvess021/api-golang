d6b4fc405ce051874de05802de9d9dc0ae33fb9a
jalvess021 <jalvess021@gmail.com>
2024-09-09 11:09:27 -0300

    ajustando o inicializador da api

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/jalvess021/api-golang/db" // Atualize o caminho conforme a sua estrutura de diretórios
+	"encoding/json"
+	"net/http"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/jalvess021/api-golang/internal/db" // Atualize o caminho conforme a sua estrutura de diretórios
+	"github.com/jalvess021/api-golang/internal/infra/akafka"
+	"github.com/go-chi/chi/v5"
+	"github.com/jalvess021/api-golang/internal/infra/web"
+	"github.com/jalvess021/api-golang/internal/usecase/product" 
+	"github.com/jalvess021/api-golang/internal/infra/repository/pgsql"
 )
 
 func main() {
-	db.ConnectDb()
 
-	// Inicializa o roteador Gin
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8282")
+	db, err := db.ConnectDb();
+	if  err != nil {
+		panic(err)
+	}
+	defer db.Close();
+
+	r := chi.NewRouter()
+	
+	// chama as rotas
+	web.SetupRoutes(r, db);
+	go http.ListenAndServe(":8282", r)
+	
+	msgChan := make(chan *kafka.Message)
+	go akafka.Consume([]string{"product"}, "kafka:9092", msgChan)
+
+	//Cria repositorio e casos de usuo
+	repository := repository.NewProductRepositoryPgsql(db)
+	createProductUseCase := usecase.NewCreateProductUseCase(repository)
+
+	//Acessando por dto
+	for msg := range msgChan {
+		dto := usecase.CreateProductInputDto{}
+		err := json.Unmarshal(msg.Value, &dto)
+		if err != nil {
+			//logar erro
+		}
+		_, err = createProductUseCase.Execute(dto)
+	}
 }
